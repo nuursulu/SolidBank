b268927ee720d9ec67f0431f3c70a88890aa1fc9
Nursulu Ospan <nursulu.ospan@jits-3-25.jits.ipa.dc>
2022-05-10 18:03:47 +0600

    2nd commit

--- src/main/java/com/example/mycli/service/AccountCreationServiceImpl.java
+++ src/main/java/com/example/mycli/service/AccountCreationServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.mycli.service;
 import com.example.mycli.entity.Account;
 import com.example.mycli.entity.AccountType;
 import com.example.mycli.DAO.AccountDAO;
+import com.example.mycli.entity.FixedAccount;
 import com.example.mycli.service.AccountCreationService;
 
 public class AccountCreationServiceImpl implements AccountCreationService {
@@ -14,7 +15,15 @@ public class AccountCreationServiceImpl implements AccountCreationService {
 
     @Override
     public void create(AccountType accountType, long bankID, String clientID, long accountID) {
-        //String accountNumber = String.format("%03d%06d", 1, accountID);
+        String accountNumber = String.format("%03d%06d", 1, accountID);
+        Account account;
+        if (accountType == AccountType.FIXED) {
+             account = new FixedAccount(accountType, accountNumber, clientID, 0, false);
+        }
+        accountDAO.createNewAccount(account);
+
+    }
+
 
     }
 }
